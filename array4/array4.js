a826b6e3fe411d0053781c1d548c43a78187b1d9
Vinicius Victor <vine.victordev@gmail.com>
2022-10-24 19:39:58 -0300

    Correção Exercicio 3

--- array4/array4.js
+++ array4/array4.js
@@ -40,64 +40,63 @@
 
 // Exercicio 3
 
-// var arrayA = []
-// var arrayB = []
-// var arrayC = []
-// var indexArray = 0
+var arrayA = []
+var arrayB = []
+var arrayC = []
+var indexArray = 0
 
-// var repeat = true
-// while (repeat) {
-//     arrayA[indexArray] = parseInt(prompt("Vetor A:"))
-//     arrayB[indexArray] = parseInt(prompt("Vetor B:"))
-//     var continuar = prompt("Deseja Adicionar mais Numeros? s/n")
+var repeat = true
+while (repeat) {
+    arrayA[indexArray] = parseInt(prompt("Vetor A:"))
+    arrayB[indexArray] = parseInt(prompt("Vetor B:"))
+    var continuar = prompt("Deseja Adicionar mais Numeros? s/n")
 
-//     if (continuar == "n" || continuar == "N") {
-//         repeat = false
-//     }
-//     indexArray++
-// }
+    if (continuar == "n" || continuar == "N") {
+        repeat = false
+    }
+    indexArray++
+}
 
 
-// arrayA.length
-// var indexArray = 1
-// for (i = 0; i < arrayA.length; i++) {
+arrayA.length
+var indexArray = 1
+for (i = 0; i < arrayA.length; i++) {
 
-//     if (arrayA[i] % 2 != 0 && arrayB[i] % 2 != 0) {
-//         arrayC[i] = arrayA[i]
-//         arrayC[i + indexArray] = arrayB[i]
-//         indexArray++
-//     } else if (arrayA[i] == arrayB[i] || arrayA[i] % 2 != 0) {
-//         arrayC[i] = arrayA[i]
+    if (arrayA[i] % 2 != 0 && arrayB[i] % 2 != 0) {
+        arrayC[i] = arrayA[i]
+        arrayC[i + indexArray] = arrayB[i]
+    } else if (arrayA[i] == arrayB[i] || arrayA[i] % 2 != 0) {
+        arrayC[i] = arrayA[i]
 
-//     } else if (arrayB[i] % 2 != 0) {
-//         arrayC[i] = arrayB[i]
-//     }
-// }
-// console.log(arrayA)
-// console.log(arrayB)
-// console.log(arrayC)
+    } else if (arrayB[i] % 2 != 0) {
+        arrayC[i] = arrayB[i]
+    }
+}
+console.log(arrayA)
+console.log(arrayB)
+console.log(arrayC)
 
 // Exercicio 4
 
-var arrayA = []
-var arrayB = []
+// var arrayA = []
+// var arrayB = []
 
-for (var i = 0; i < 10; i++) {
-    arrayA[i] = parseInt(prompt("Digite o " + (i + 1) + " Numero"))
-}
+// for (var i = 0; i < 10; i++) {
+//     arrayA[i] = parseInt(prompt("Digite o " + (i + 1) + " Numero"))
+// }
 
-var indexArrayB = 0
+// var indexArrayB = 0
 
-for (var i = 0; i < arrayA.length; i++) {
-    if (arrayA[i] == 0) {
-        arrayB[indexArrayB] = arrayA[i + 1]
-        indexArrayB++
-        i++
-    } else {
-        arrayB[indexArrayB] = arrayA[i]
-        indexArrayB++
-    }
+// for (var i = 0; i < arrayA.length; i++) {
+//     if (arrayA[i] == 0) {
+//         arrayB[indexArrayB] = arrayA[i + 1]
+//         indexArrayB++
+//         i++
+//     } else {
+//         arrayB[indexArrayB] = arrayA[i]
+//         indexArrayB++
+//     }
 
-}
-console.log("Entrada = " + arrayA)
-console.log("Saida = " + arrayB)
+// }
+// console.log("Entrada = " + arrayA)
+// console.log("Saida = " + arrayB)
